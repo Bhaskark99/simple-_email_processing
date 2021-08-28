aa0212e965589ff0dd1602675d8d950b6fe93e14
Bhaskark99 <89676318+Bhaskark99@users.noreply.github.com>
2021-08-28 09:49:32 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+<title>email system</title>
+
+</head>
+
+<body>
+
+<form method="post" action="validate.php">
+
+enter your name: <input type="text" name="sender_name"><br> 
+
+enter your mail: <input type="text" name="sender_mail"><br>
+
+enter your reciever mail: <input type="text" name="reci_mail"><br> 
+
+enter your subject: <input type="text" name="subject"><br>
+
+enter your message: <textarea name="message"></textarea><br>
+
+<input type="submit" name="send" value="send">
+
+</form>
+
+</body>
+
+</html>
